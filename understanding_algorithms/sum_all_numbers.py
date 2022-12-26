151abd255c4dbc6296decd30545a12ae3e49cb75
xonfps <isaquepow12@gmail.com>
2022-12-26 17:49:41 -0300

    added: function to some all numbers in list

---
+++ understanding_algorithms/sum_all_numbers.py
@@ -0,0 +1,7 @@
+def sum_all_numbers_in_the_list(list):
+    if list == []:
+        return 0
+    else:
+        return list[0] + sum_all_numbers_in_the_list(list[1:])
+    
+print(sum_all_numbers_in_the_list([4, 3, 2]))
