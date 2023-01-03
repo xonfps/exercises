dbf8dcdd66e8130e3c0e2eaaeb39da4c90837db0
xonfps <isaquepow12@gmail.com>
2023-01-03 15:55:12 -0300

    added: function for a simple loop

---
+++ understanding_algorithms/simple_loop.py
@@ -0,0 +1,6 @@
+number = 0
+while number < 10:
+    print(number)
+    number += 1
+
+print(number)
--- understanding_algorithms/sum_all_numbers.py
+++ understanding_algorithms/sum_all_numbers.py
@@ -3,5 +3,5 @@ def sum_all_numbers_in_the_list(list):
         return 0
     else:
         return list[0] + sum_all_numbers_in_the_list(list[1:])
-    
+
 print(sum_all_numbers_in_the_list([4, 3, 2]))
